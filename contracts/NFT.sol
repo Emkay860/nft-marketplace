39ee62dc101b7d7bcd7b2d9772e5c5bc938ab4d8
Majid Kareem <kareemmajid86@gmail.com>
2021-11-04 04:06:11 +0100

    Initial commit

---
+++ contracts/NFT.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+    
+    /**
+     * @dev Initializes the contract by setting a `name` and a `symbol` to the token collection.
+     *      and setting `marketplaceAddress` to `contractAddress`
+     */
+    constructor(address marketplaceAddress) ERC721("Multiverse Tokens", "MITT") {
+        contractAddress = marketplaceAddress;
+    }
+
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+        return newItemId;
+    }
+
+}
---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs");
+
+const privateKey = fs.readFileSync(".secret").toString();
+const projectId = "evXVt1m95dXpPeuei7XR7IDSJmW0Dv1sOW7xA6yh";
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://rnl9tzxcm93a.usemoralis.com:2053/server/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {}
+  },
+  solidity: "0.8.4",
+};
