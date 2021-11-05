5ef6b79d1ceacaf7136ffb1f9d94db24e9e8fbe6
Majid Kareem <kareemmajid86@gmail.com>
2021-11-06 00:24:15 +0100

    Deployed to mumbai testnet
    Uppdated contract address and JsonRpc to mumbai testnet

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftaddress = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707";
-export const nftmarketaddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
+export const nftaddress = '0x755a0C243bC33B92f3A2acEB5a0f62f5Ff46773a';
+export const nftmarketaddress = '0xEEBB4ab9B263adAd8b438010D34121A544EAbf86';
--- pages/create-item.js
+++ pages/create-item.js
@@ -13,111 +13,120 @@ import NFT from '../artifacts/contracts/NFT.sol/NFT.json';
 import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json';
 
 export default function CreateItem() {
-    const [fileUrl, setFileUrl] = useState(null);
-    const [formInput, updateFormInput] = useState({ price: '', name: '', description: '' });
-    const router = useRouter();
+  const [fileUrl, setFileUrl] = useState(null);
+  const [formInput, updateFormInput] = useState({
+    price: '',
+    name: '',
+    description: '',
+  });
+  const router = useRouter();
 
-    async function onChange(e) {
-        const file = e.target.files[0];
-        try {
-            const added = await client.add(
-                file, {
-                    progress: (prog) => console.log(`received: ${prog}`)
-                }
-            )
-            const url = `https://ipfs.infura.io/ipfs/${added.path}`;
-            setFileUrl(url);
-            
-        } catch (error) {
-            console.log(error)
-        }
+  async function onChange(e) {
+    const file = e.target.files[0];
+    try {
+      const added = await client.add(file, {
+        progress: (prog) => console.log(`received: ${prog}`),
+      });
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+      setFileUrl(url);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function createItem() {
+    const { name, description, price } = formInput;
+    if (!name || !description || !price || !fileUrl) {
+      return;
     }
 
-    async function createItem() {
-        const { name, description, price } = formInput;
-        if(!name || !description || !price || !fileUrl) {
-            return
-        }
-        
-        const data = JSON.stringify({
-            name, description, image: fileUrl
-        })
+    const data = JSON.stringify({
+      name,
+      description,
+      image: fileUrl,
+    });
 
-        try {
-            const added = await client.add(data);
-            const url = `https://ipfs.infura.io/ipfs/${added.path}`;
-            console.log(url);
-            createSale(url);
-            
-        } catch (error) {
-            console.log('Error uploading file: ', error);
-        }
-
+    try {
+      const added = await client.add(data);
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+      console.log(url);
+      createSale(url);
+    } catch (error) {
+      console.log('Error uploading file: ', error);
     }
+  }
 
-    async function createSale(url) {
-        const web3Modal = new Web3Modal();
-        const connection = await web3Modal.connect();
-        const provider = new ethers.providers.Web3Provider(connection);
-        const signer = provider.getSigner();
+  async function createSale(url) {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
 
-        // creates a new nft
-        let contract = new ethers.Contract(nftaddress, NFT.abi, signer);
-        let transaction = await contract.createToken(url);
-        let tx = await transaction.wait();
+    // creates a new nft
+    let contract = new ethers.Contract(nftaddress, NFT.abi, signer);
+    let transaction = await contract.createToken(url);
+    let tx = await transaction.wait();
 
-        let event = tx.events[0];
-        let value = event.args[2];
-        let tokenId = value.toNumber();
+    let event = tx.events[0];
+    let value = event.args[2];
+    let tokenId = value.toNumber();
 
-        const price = ethers.utils.parseUnits(formInput.price, 'ether');
+    const price = ethers.utils.parseUnits(formInput.price, 'ether');
 
-        // list the new nft to the marketplace
-        contract = new ethers.Contract(nftmarketaddress, Market.abi, signer);
-        let listingPrice = await contract.getListingPrice();
-        listingPrice = listingPrice.toString();
+    // list the new nft to the marketplace
+    contract = new ethers.Contract(nftmarketaddress, Market.abi, signer);
+    let listingPrice = await contract.getListingPrice();
+    listingPrice = listingPrice.toString();
 
-        transaction = await contract.createMarketItem(nftaddress, tokenId, price, { value: listingPrice });
-        await transaction.wait();
+    transaction = await contract.createMarketItem(nftaddress, tokenId, price, {
+      value: listingPrice,
+    });
+    await transaction.wait();
 
-        router.push('/');
-    }
+    router.push('/');
+  }
 
-    return (
-        <div className="flex justify-center">
-          <div className="w-1/2 flex flex-col pb-12">
-            <input 
-              placeholder="Asset Name"
-              className="mt-8 border rounded p-4"
-              onChange={e => updateFormInput({ ...formInput, name: e.target.value })}
-            />
-            <textarea
-              placeholder="Asset Description"
-              className="mt-2 border rounded p-4"
-              onChange={e => updateFormInput({ ...formInput, description: e.target.value })}
-            />
-            <input
-              placeholder="Asset Price in Matic"
-              className="mt-2 border rounded p-4"
-              onChange={e => updateFormInput({ ...formInput, price: e.target.value })}
-            />
-            <input
-              type="file"
-              name="Asset"
-              className="my-4"
-              onChange={onChange}
-            />
-            {
-              fileUrl && (
-                <Image className="rounded mt-4" src={fileUrl} alt="" />
-              )
-            }
-            <button onClick={createItem} className="font-bold mt-4 bg-purple-500 text-white rounded p-4 shadow-lg hover:bg-purple-600">
-              Create Digital Asset
-            </button>
-          </div>
-        </div>
-      )
-
+  return (
+    <div className="flex justify-center">
+      <div className="w-1/2 flex flex-col pb-12">
+        <input
+          placeholder="Asset Name"
+          className="mt-8 border rounded p-4"
+          onChange={(e) =>
+            updateFormInput({ ...formInput, name: e.target.value })
+          }
+        />
+        <textarea
+          placeholder="Asset Description"
+          className="mt-2 border rounded p-4"
+          onChange={(e) =>
+            updateFormInput({ ...formInput, description: e.target.value })
+          }
+        />
+        <input
+          placeholder="Asset Price in Matic"
+          className="mt-2 border rounded p-4"
+          onChange={(e) =>
+            updateFormInput({ ...formInput, price: e.target.value })
+          }
+        />
+        <input type="file" name="Asset" className="my-4" onChange={onChange} />
+        {fileUrl && (
+          <Image
+            className="rounded mt-4"
+            width="200"
+            height="200"
+            src={fileUrl}
+            alt=""
+          />
+        )}
+        <button
+          onClick={createItem}
+          className="font-bold mt-4 bg-purple-500 text-white rounded p-4 shadow-lg hover:bg-purple-600"
+        >
+          Create Digital Asset
+        </button>
+      </div>
+    </div>
+  );
 }
-
